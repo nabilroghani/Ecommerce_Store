c7d94aa15f8bce46eff43b7af6eda235b50575b0
Nabil Ahmad <nabilroghani92@gmail.com>
2026-02-04 16:54:39 +0500

    update

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,22 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const { route } = require("./routes/user.routes");
 
-
 const app = express();
 
-app.use(express.json());
 connectDB();
-app.use(cors());
+
+app.use(cors()); 
+app.use(express.json());
 
 app.use("/api", route);
 
-app.listen(process.env.PORT, () => {
-  console.log(`server running on http://localhost:${process.env.PORT}`);
+app.get("/", (req, res) => {
+  res.send("Fashion Fusion API is running...");
 });
+
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
